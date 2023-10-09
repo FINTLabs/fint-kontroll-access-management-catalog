3781fab2007aea4db8c6d18a7e711104b7584c39
Morten Solberg <morten@source-code.no>
2023-10-09 10:29:12 +0200

    FKS-117: Add roles to db initialization

---
+++ src/main/resources/db/migration/V2__add_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO accessrole (access_role_id, name) VALUES ('ata', 'applikasjonstilgangsadministrator');
+INSERT INTO accessrole (access_role_id, name) VALUES ('aa', 'applikasjonsadministrator');
+INSERT INTO accessrole (access_role_id, name) VALUES ('al', 'enhetsleder');
+INSERT INTO accessrole (access_role_id, name) VALUES ('sb', 'sluttbruker');
+
