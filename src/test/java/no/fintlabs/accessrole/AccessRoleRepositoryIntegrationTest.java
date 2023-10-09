d3cf3c14af882c19ce615a19f5381de3f3527d7e
Morten Solberg <morten@source-code.no>
2023-10-09 15:59:11 +0200

    FKS-121: Add testcontroller test for postgres

---
+++ src/test/java/no/fintlabs/accessrole/AccessRoleRepositoryIntegrationTest.java
@@ -0,0 +1,75 @@
+package no.fintlabs.accessrole;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@Testcontainers
+public class AccessRoleRepositoryIntegrationTest {
+
+    @Container
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("test")
+            .withUsername("user")
+            .withPassword("password");
+
+    @Autowired
+    private AccessRoleRepository accessRoleRepository;
+
+    @DynamicPropertySource
+    static void registerPgProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.flyway.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.flyway.user", postgreSQLContainer::getUsername);
+        registry.add("spring.flyway.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeEach
+    void setup() {
+        // Initialize test data
+        AccessRole role1 = AccessRole.builder()
+                .accessRoleId("AR1")
+                .name("Role1")
+                .build();
+
+        AccessRole role2 = AccessRole.builder()
+                .accessRoleId("AR2")
+                .name("Role2")
+                .build();
+
+        accessRoleRepository.save(role1);
+        accessRoleRepository.save(role2);
+    }
+
+    @Test
+    void shouldFindAllRoles() {
+        List<AccessRole> roles = accessRoleRepository.findAll();
+
+        assertEquals(2, roles.size());
+
+        AccessRole role1 = roles.get(0);
+        assertThat(role1.getAccessRoleId()).isEqualTo("AR1");
+        assertThat(role1.getName()).isEqualTo("Role1");
+
+        AccessRole role2 = roles.get(1);
+        assertThat(role2.getAccessRoleId()).isEqualTo("AR2");
+        assertThat(role2.getName()).isEqualTo("Role2");
+
+    }
+}
+
--- src/test/java/no/fintlabs/accessrole/AccessRoleRepositoryJdbcTest.java
+++ src/test/java/no/fintlabs/accessrole/AccessRoleRepositoryJdbcTest.java
@@ -1,9 +1,6 @@
 package no.fintlabs.accessrole;
 
-import no.fintlabs.accessrole.AccessRole;
-import no.fintlabs.accessrole.AccessRoleRepository;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -15,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
-public class AccessRoleRepositoryTest {
+public class AccessRoleRepositoryJdbcTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
