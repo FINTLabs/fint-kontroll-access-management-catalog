e369305f1935e0c25c424cdc816f99126df99ce7
Morten Solberg <morten@source-code.no>
2023-10-27 07:49:01 +0200

    FKS-172: Cleaning up the code. Test for OpaController.

--- src/main/java/no/fintlabs/opa/OpaController.java
+++ src/main/java/no/fintlabs/opa/OpaController.java
@@ -1,9 +1,11 @@
 package no.fintlabs.opa;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +13,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.MalformedURLException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @Slf4j
+@Tag(name = "OPA", description = "OPA API for bundle")
 @RestController
 @RequestMapping("/api/accessmanagement/v1/opabundle")
 public class OpaController {
 
-    @ApiResponse(description = "Hent OPA bundle")
-    @GetMapping
-    public ResponseEntity<Resource> getOpaBundle() {
+    @Autowired
+    private OpaBundleService opaBundleService;
+
+    @Operation(summary = "Hent OPA bundle", description = "Hent OPA bundle")
+    @ApiResponse(responseCode = "200", description = "OK")
+    @GetMapping(produces = "application/gzip")
+    public ResponseEntity<Resource> getOpaBundle() throws Exception {
         try {
-            log.info("Getting OPA bundle");
-            Path file = Paths.get(System.getProperty("java.io.tmpdir") + "/opabundle/", "opabundle.tar.gz");
-            Resource resource = new UrlResource(file.toUri());
+            Resource resource = opaBundleService.getOpaBundleFile();
+
+            log.info("Getting OPA bundle from {}", resource.getURI());
 
             if (resource.exists()) {
-                log.info("Found OPA bundle, returning");
                 return ResponseEntity.ok()
                         .contentType(MediaType.parseMediaType("application/gzip"))
                         .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                         .body(resource);
             } else {
-                log.info("OPA bundle not found");
+                log.error("OPA bundle not found");
                 return ResponseEntity.notFound().build();
             }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Error reading the bundle file!", e);
+        } catch (Exception e) {
+            log.error("Error reading the bundle file!", e);
+            throw e;
         }
     }
+
+
 
 }
