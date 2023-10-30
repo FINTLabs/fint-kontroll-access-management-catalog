3c6c6572228b610b134230f5db85ab7a5b0790ce
Morten Solberg <morten@source-code.no>
2023-10-30 07:42:28 +0100

    FKS-172: Sql for manually adding the teams users

---
+++ src/main/resources/create-tmp-vigo-users.sql
@@ -0,0 +1,43 @@
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo1', 'erling.jahr@vigoiks.no', 'Erling', 'Jahr');
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo2', 'morten.solberg@vigoiks.no', 'Morten', 'Solberg');
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo3', 'linda.loftsgarden@vigoiks.no', 'Linda', 'Loftsgarden');
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo4', 'jennifer.strand@vigoiks.no', 'Jennifer', 'Strand');
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo5', 'christian.haugland@vigoiks.no', 'Christian', 'Haugland');
+INSERT INTO accessuser (resource_id, user_name, first_name, last_name) VALUES ('vigo6', 'berntj@viken.no', 'Bernt', 'Johansen');
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo1', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo1', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo1', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo1', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo1', 'ata', 5);
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo2', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo2', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo2', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo2', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo2', 'ata', 5);
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo3', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo3', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo3', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo3', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo3', 'ata', 5);
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo4', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo4', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo4', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo4', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo4', 'ata', 5);
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo5', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo5', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo5', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo5', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo5', 'ata', 5);
+
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo6', 'ata', 1);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo6', 'ata', 2);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo6', 'ata', 3);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo6', 'ata', 4);
+INSERT INTO accessassignment (user_id, access_role_id, scope_id) VALUES ('vigo6', 'ata', 5);
+
