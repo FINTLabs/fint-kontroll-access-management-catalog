75c677b63a20486975f1c2c94b8f980a8c85b1b6
Morten Solberg <morten@source-code.no>
2023-11-22 19:04:04 +0100

    FKS-268: File missed by commit

--- src/main/java/no/fintlabs/accessassignment/AccessAssignmentController.java
+++ src/main/java/no/fintlabs/accessassignment/AccessAssignmentController.java
@@ -6,8 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.accessassignment.repository.AccessAssignment;
 import no.fintlabs.accessrole.AccessRole;
 import no.fintlabs.accessrole.AccessRoleRepository;
-import no.fintlabs.scope.repository.Scope;
-import no.fintlabs.scope.repository.ScopeRepository;
 import no.fintlabs.user.repository.AccessUser;
 import no.fintlabs.user.repository.AccessUserRepository;
 import org.springframework.http.HttpStatus;
@@ -19,6 +17,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/accessmanagement/v1/accessassignment")
@@ -27,44 +27,38 @@ public class AccessAssignmentController {
     private final AccessAssignmentService accessAssignmentService;
     private final AccessUserRepository accessUserRepository;
     private final AccessRoleRepository accessRoleRepository;
-    private final ScopeRepository scopeRepository;
 
     public AccessAssignmentController(AccessAssignmentService accessAssignmentService,
                                       AccessUserRepository accessUserRepository,
-                                      AccessRoleRepository accessRoleRepository,
-                                      ScopeRepository scopeRepository) {
+                                      AccessRoleRepository accessRoleRepository) {
         this.accessAssignmentService = accessAssignmentService;
         this.accessUserRepository = accessUserRepository;
         this.accessRoleRepository = accessRoleRepository;
-        this.scopeRepository = scopeRepository;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public AccessAssignmentDto createAccessAssignment(@RequestBody @Valid AccessAssignmentDto accessAssignmentDto) {
+    public List<AccessAssignmentDto> createAccessAssignments(@RequestBody @Valid UpdateAccessAssignmentDto accessAssignmentDto) {
         log.info("Assigning access to user with data {}", accessAssignmentDto);
 
         try {
-
             AccessUser accessUser = accessUserRepository.findById(accessAssignmentDto.userId())
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist"));
 
             AccessRole accessRole = accessRoleRepository.findById(accessAssignmentDto.accessRoleId())
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role does not exist"));
 
-            Scope scope = scopeRepository.findById(accessAssignmentDto.scopeId())
-                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Scope does not exist"));
+            // TODO: rewrite accessAssignmentDto.scopeId() to being objecttype
 
-            AccessAssignment savedAssignment =
-                    accessAssignmentService.createAssignment(accessAssignmentDto.orgUnitIds(), scope, accessUser, accessRole);
+            List<AccessAssignment> savedAssignments =
+                    accessAssignmentService.createAssignmentsForUser(accessAssignmentDto.orgUnitIds(), "", accessUser, accessRole);
 
-            return AccessAssignmentMapper.toDto(savedAssignment);
+            return AccessAssignmentMapper.toDto(savedAssignments);
 
         } catch (Exception e) {
             log.error("Error creating accessassignment {}", accessAssignmentDto, e);
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something went wrong when creating accessassignment");
         }
     }
-
 }
